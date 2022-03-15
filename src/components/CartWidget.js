511a83a9dd231dbdab2b559dde59e08867847216
guidohc98 <guidohertz98@gmail.com>
2022-03-14 22:43:06 -0300

    NavBar.js

---
+++ src/components/CartWidget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { icons } from 'react-icons';
+import { FaShoppingCart } from 'react-icons/fa';
+
+const CartWidget = () => {
+    return (
+         <button><FaShoppingCart /></button>
+    )
+}
+
+export default CartWidget;
+
+
